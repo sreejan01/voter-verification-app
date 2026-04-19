64015c6f22454a7c85ed211a2d4bb8d5efdcfb7e
Sreejan Verma <sreejanverma2004@gmail.com>
2026-04-19 18:26:10 +0530

    removed dotenv from js

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,16 +1,13 @@
 class ApiService {
-  // 👇 Change this one line only
-  static const bool _isEmulator = true; // true = emulator, false = real device
-
-  static String get baseUrl => _isEmulator
-      ? "http://10.0.2.2:5000" // emulator
-      : "http://10.71.21.217:5000"; // real device
+  // ✅ Add https:// at the start
+  static const String baseUrl =
+      "https://voter-verification-backend-production.up.railway.app";
 
   static String get login => "$baseUrl/login";
   static String get verifyOtp => "$baseUrl/verify-otp";
   static String get resendOtp => "$baseUrl/resend-otp";
   static String get verifyVoter => "$baseUrl/verify-voter";
   static String get verificationHistory => "$baseUrl/verification-history";
-  static String get searchVoter => "$baseUrl/search-voter";
   static String get dashboardStats => "$baseUrl/dashboard-stats";
+  static String get searchVoter => "$baseUrl/search-voter";
 }
